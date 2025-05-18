11d8aa8d4e374f7628090dfe4446d52250727ee3
ernat89 <eren.aykanat.ea@gmail.com>
2025-05-18 16:46:07 +0200

    Update mailer.js

--- backend/mailer.js
+++ backend/mailer.js
@@ -1,1 +1,29 @@
-// Nodemailer Setup kommt hier rein
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 587,
+  auth: {
+    user: "DEIN_MAILTRAP_USER",
+    pass: "DEIN_MAILTRAP_PASS"
+  }
+});
+
+function sendConfirmationMail(email, name, date, start, end) {
+  const message = {
+    from: "kalender@deine-domain.de",
+    to: email,
+    subject: "Deine Buchung beim Besuchskalender",
+    text: `Hallo ${name},\n\ndu hast einen Besuch gebucht am ${date} von ${start} bis ${end}.\n\nBis bald!`
+  };
+
+  transporter.sendMail(message, (err, info) => {
+    if (err) {
+      console.error("Mail Fehler:", err);
+    } else {
+      console.log("Bestätigungsmail gesendet:", info.messageId);
+    }
+  });
+}
+
+module.exports = { sendConfirmationMail };
